38ff04a6b12ca040ffeb8230839ab13da362ebf4
jonas <jonas@example.com>
2025-02-12 22:28:22 +0100

    Restore focused item, when coming back from another view

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/channels/ChannelsScreen.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/channels/ChannelsScreen.kt
@@ -1,6 +1,5 @@
 package com.google.jetstream.presentation.screens.channels
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Box
@@ -12,11 +11,10 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -24,8 +22,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -42,10 +42,9 @@ import com.google.jetstream.presentation.utils.GradientBg
 fun ChannelsScreen(
     gridColumns: Int = 4,
     onChannelClick: (channel: Channel) -> Unit,
-    onScroll: (isTopBarVisible: Boolean) -> Unit,
+    //onScroll: (isTopBarVisible: Boolean) -> Unit,
     channelsScreenViewModel: ChannelsScreenViewModel = hiltViewModel()
 ) {
-
     val uiState by channelsScreenViewModel.uiState.collectAsStateWithLifecycle()
 
     when (val s = uiState) {
@@ -54,11 +53,12 @@ fun ChannelsScreen(
         }
 
         is ChannelsScreenUiState.Ready -> {
+            val channelList = remember { s.channelList }
             Catalog(
                 gridColumns = gridColumns,
-                channels = s.channelList,
+                channels = channelList,
                 onChannelClick = onChannelClick,
-                onScroll = onScroll,
+                //onScroll = onScroll,
                 modifier = Modifier.fillMaxSize()
             )
         }
@@ -72,20 +72,12 @@ private fun Catalog(
     modifier: Modifier = Modifier,
     gridColumns: Int = 4,
     onChannelClick: (channel: Channel) -> Unit,
-    onScroll: (isTopBarVisible: Boolean) -> Unit,
 ) {
     val childPadding = rememberChildPadding()
     val lazyGridState = rememberLazyGridState()
-    val shouldShowTopBar by remember {
-        derivedStateOf {
-            lazyGridState.firstVisibleItemIndex == 0 &&
-                    lazyGridState.firstVisibleItemScrollOffset < 100
-        }
-    }
-    LaunchedEffect(shouldShowTopBar) {
-        onScroll(shouldShowTopBar)
-    }
 
+
+    var lastFocusedChannel by rememberSaveable { mutableStateOf<Int?>(null) }
     AnimatedContent(
         targetState = channels,
         modifier = Modifier
@@ -100,21 +92,37 @@ private fun Catalog(
         ) {
             itemsIndexed(it) { index, channel ->
                 var isFocused by remember { mutableStateOf(false) }
+                val focusRequester = remember { FocusRequester() }
+                val isItemToFocus = lastFocusedChannel == channel.hashCode()
                 MovieCard(
+
                     onClick = {
                         onChannelClick(channel)
+                        isFocused = true
+                        lastFocusedChannel = channel.hashCode()
                     },
                     modifier = Modifier
                         .padding(8.dp)
                         .aspectRatio(16 / 9f)
                         .onFocusChanged {
                             isFocused = it.isFocused || it.hasFocus
+                            if (isFocused && !isItemToFocus) {
+                                lastFocusedChannel = null
+                            }
                         }
                         .focusProperties {
                             if (index % gridColumns == 0) {
                                 left = FocusRequester.Cancel
                             }
                         }
+                        .focusRequester(focusRequester)
+                        .onGloballyPositioned {
+                            if(isItemToFocus) {
+                                focusRequester.requestFocus()
+                            }
+
+                        }
+
                 ) {
                     val itemAlpha by animateFloatAsState(
                         targetValue = if (isFocused) .6f else 0.2f,
